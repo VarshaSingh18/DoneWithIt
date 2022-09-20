a0a113ca0414048e0d4e65227da9c6561ac56575
Varsha Singh <varshasinghparihar123@gmail.com>
2022-09-20 20:49:03 +0530

    Tochables

--- App.js
+++ App.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image, SafeAreaView } from 'react-native';
+import { StyleSheet, Text, View, Image,TouchableWithoutFeedback, TouchableOpacity, TouchableHighlight,TouchableNativeFeedback,SafeAreaView } from 'react-native';
 
 export default function App() {
   console.log(require("./assets/favicon.png"));
   return (
     <SafeAreaView style={styles.container}>
       <Text>Hello World!</Text>
-      <Image blurRadius={10} source={{
+
+      <TouchableOpacity onPress={()=>console.log("Image Tapped")}>
+      <Image source={{
         width : 200,
         height :300,
         uri : "https://picsum.photos/200/300"}}/>
+        </TouchableOpacity>
+
       <StatusBar style="auto" />
     </SafeAreaView>
   );
