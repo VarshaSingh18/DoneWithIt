05e53f52f254134861c5f9965babb9c2e0405849
Varsha Singh <varshasinghparihar123@gmail.com>
2022-09-20 22:03:35 +0530

    Button & Alert

--- App.js
+++ App.js
@@ -1,21 +1,13 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image,TouchableWithoutFeedback, TouchableOpacity, TouchableHighlight,TouchableNativeFeedback,SafeAreaView } from 'react-native';
+import { StyleSheet,SafeAreaView, Button } from 'react-native';
+import { wView } from 'react-native-web';
 
 export default function App() {
   console.log(require("./assets/favicon.png"));
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Hello World!</Text>
-
-      <TouchableOpacity onPress={()=>console.log("Image Tapped")}>
-      <Image source={{
-        width : 200,
-        height :300,
-        uri : "https://picsum.photos/200/300"}}/>
-        </TouchableOpacity>
-
-      <StatusBar style="auto" />
+      <Button title="Click me" onPress={()=>{alert("Button tapped"), console.log("Button tapped")}}/>
     </SafeAreaView>
   );
 }
