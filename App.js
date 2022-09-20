52873efcdf0096ed8a5361a80d0cb791c16beab6
Varsha Singh <varshasinghparihar123@gmail.com>
2022-09-20 22:16:11 +0530

    Used Alert.alert

--- App.js
+++ App.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet,SafeAreaView, Button } from 'react-native';
+import { StyleSheet,SafeAreaView, Button, Alert } from 'react-native';
 import { wView } from 'react-native-web';
 
 export default function App() {
   console.log(require("./assets/favicon.png"));
   return (
     <SafeAreaView style={styles.container}>
-      <Button title="Click me" onPress={()=>{alert("Button tapped"), console.log("Button tapped")}}/>
+      <Button title="Click me" onPress={()=>{
+        Alert.alert("My Title", "My Message", [
+          {text: "Yes", onPress:()=>{console.log("Yes")}},
+          {text: "No", onPress:()=>{console.log("No")}}
+        ])
+      }}/>
     </SafeAreaView>
   );
 }
