2f34c49d1ba13e5033f94da6e0f3ecfad5b17b25
Varsha Singh <varshasinghparihar123@gmail.com>
2022-09-21 10:41:52 +0530

    Flex-Direction

--- App.js
+++ App.js
@@ -1,36 +1,33 @@
 import React from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet,
-         SafeAreaView,
-         Platform, 
-         Dimensions,
-          View} from 'react-native';
-
-import {useDimensions,useDeviceOrientation } from '@react-native-community/hooks';
+import {View} from 'react-native';
 
 export default function App() {
-  const { landscape }=useDeviceOrientation();
-  return (
-    <SafeAreaView style={styles.container}>
-      <View 
-      style={{
-       backgroundColor : "dodgerblue",
-       width : "100%",
-       height : landscape ? '100%' : '30%'
-      }}
-      >
 
-      </View>
-    </SafeAreaView>
+  return(
+   <View style={{
+     backgroundColor : "#fff",
+     flex: 1,
+     flexDirection: "row-reverse"
+   }}>
+       <View style ={{
+         backgroundColor: "gold",
+         width:100,
+         height:100
+       }}/>
+
+       <View style ={{
+         backgroundColor: "dodgerblue",
+         width:100,
+         height:100
+       }}/>
+       <View style ={{
+         backgroundColor: "tomato",
+         width:100,
+         height:100
+       }}/>
+     </View>
+
+ 
   );
+
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    paddingTop: Platform.OS=== "android" ? StatusBar.currentHeight : 0
-  },
-});
-
-/*In portrait mode you wanna have this video player on top, In landscape you wanna to this entire screen */
