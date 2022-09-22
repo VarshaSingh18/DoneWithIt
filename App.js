9691ad289984cb93724bbaadd3124b9359d2d23c
Varsha Singh <varshasinghparihar123@gmail.com>
2022-09-23 00:13:11 +0530

    Added background image

--- App.js
+++ App.js
@@ -1,42 +1,11 @@
 import React from 'react';
 import {View} from 'react-native';
+import WelcomeScreen from './app/screens/WelcomeScreen';
 
 export default function App() {
 
   return(
-   <View style={{
-     backgroundColor : "#fff",
-     flex: 1,
-     flexDirection: "row-reverse",//horizontal
-     justifyContent: "center",  //main
-     alignItems: "center", // verticle
-     alignContent:"center",
-     flexwrap: "wrap"
-   }}>
-       <View style ={{
-         backgroundColor: "gold",
-         width:100,
-         height:100,
-       }}/>
-
-       <View style ={{
-         backgroundColor: "dodgerblue",
-         width:100,
-         height:100
-       }}/>
-       <View style ={{
-         backgroundColor: "tomato",
-         width:100,
-         height:100
-       }}/>
-       <View style ={{
-         backgroundColor: "green",
-         width:100,
-         height:100
-       }}/>
-     </View>
-
- 
+  <WelcomeScreen/>
   );
 
 }
