de80acc75c41ce798e07864801b7801b5039f7f7
Varsha Singh <varshasinghparihar123@gmail.com>
2022-09-29 00:16:41 +0530

    MaterialCommunityIcons in ViewScreenItems

--- App.js
+++ App.js
@@ -3,11 +3,12 @@ import {View,Text} from 'react-native';
 import WelcomeScreen from './app/screens/WelcomeScreen';
 import Card from './app/components/Card';
 import ListingDetailsScreen from './app/screens/ListingDetailsScreen';
+import ViewImageScreen from './app/screens/ViewImageScreen';
 export default function App() {
 
   return(
 
- <ListingDetailsScreen/>
+ <ViewImageScreen/>
   );
 
 }
